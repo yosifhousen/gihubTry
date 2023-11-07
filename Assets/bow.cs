cc010bd85800a9a2be9b85627c3deae1d33f26e0
yosifhousen <yosif.hosen@e.braude.ac.il>
2023-11-07 15:20:05 +0200

    2 commit

---
+++ Assets/bow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bow : MonoBehaviour
+{
+    public GameObject arrow;
+    public float launchForce;
+    public Transform shotPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 bowPosition = transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - bowPosition;
+        transform.right = -direction;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+       GameObject newArrow = Instantiate(arrow, shotPoint.position, shotPoint.rotation);
+        newArrow.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce*-1;
+    }
+}
